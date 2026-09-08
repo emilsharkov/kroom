1eb04f071d76127897b3b164c6824d6d97d6b63b
Emil Sharkov <emosharkov@gmail.com>
2026-09-08 15:59:44 -0700

    add userpgrepo

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,10 @@
 use std::error::Error;
 
 use::syringe::container::Container;
-use syringe::test::user_repo::{UserMockRepo, UserRepo};
+use syringe::test::user_repo::{UserMockRepo, UserPgRepo, UserRepo};
 
 fn main() -> Result<(),Box<dyn Error>> {
     let mut container: Container = Container::new();
-    let _s = container.get::<UserMockRepo>();
 
     container.register::<dyn UserRepo,UserMockRepo>();
     let user_repo = container.get::<dyn UserRepo>();
@@ -16,6 +15,11 @@ fn main() -> Result<(),Box<dyn Error>> {
     let mock_user_repo = container.get::<UserMockRepo>();
     let user2 = mock_user_repo.find_one("".to_string());
     println!("{:?}",user2);
+
+    container.register::<UserPgRepo,UserPgRepo>();
+    let pg_user_repo = container.get::<UserPgRepo>();
+    let user3 = pg_user_repo.find_one("".to_string());
+    println!("{:?}",user3);
 
     Ok(())
 }
--- src/test/user_repo.rs
+++ src/test/user_repo.rs
@@ -10,8 +10,8 @@ pub struct UserMockRepo {}
 impl UserRepo for UserMockRepo {
     fn find_one(&self, _user_id: String) -> User {
         User {
-            id: "123-456".to_string(),
-            name: "Emil".to_string()
+            id: "mock-id".to_string(),
+            name: "mock".to_string()
         }
     }
 }
@@ -31,5 +31,33 @@ impl Injectable<UserMockRepo> for UserMockRepo {
     ) -> Arc<UserMockRepo> {
         let user_mock_repo: UserMockRepo = Self {};
         Arc::new(user_mock_repo)
+    }
+}
+
+pub struct UserPgRepo {}
+impl UserRepo for UserPgRepo {
+    fn find_one(&self, _user_id: String) -> User {
+        User {
+            id: "pg-id".to_string(),
+            name: "pg".to_string()
+        }
+    }
+}
+
+impl Injectable<dyn UserRepo> for UserPgRepo {
+    fn __syringe_construct(
+        _container: &Container
+    ) -> Arc<dyn UserRepo> {
+        let user_pg_repo: UserPgRepo = Self {};
+        Arc::new(user_pg_repo)
+    }
+}
+
+impl Injectable<UserPgRepo> for UserPgRepo {
+    fn __syringe_construct(
+        _container: &Container
+    ) -> Arc<UserPgRepo> {
+        let user_pg_repo: UserPgRepo = Self {};
+        Arc::new(user_pg_repo)
     }
 }
