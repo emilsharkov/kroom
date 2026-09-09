e8978d6b554c11634079e807e89b6a2930b2ccd7
Emil Sharkov <emosharkov@gmail.com>
2026-09-08 21:24:36 -0700

    multi inject foundation laid

--- src/container.rs
+++ src/container.rs
@@ -1,12 +1,12 @@
 use std::collections::HashMap;
-use std::any::{Any, TypeId, type_name};
+use std::any::{Any, TypeId, type_name, type_name_of_val};
 use std::sync::Arc;
 use crate::injectable::Injectable;
 
 type Constructor = fn(&Container) -> Box<dyn Any>;
 
 pub struct Container {
-    interface_constructor_registry: HashMap<TypeId, Constructor>,
+    interface_constructor_registry: HashMap<TypeId, Vec<Constructor>>,
 }
 
 impl Container {
@@ -15,35 +15,101 @@ impl Container {
             interface_constructor_registry: HashMap::new(),
         }
     }
-
-    pub fn get<Interface: ?Sized + 'static>(&self) -> Arc<Interface> {
-        let interface_id: TypeId = TypeId::of::<Interface>();
-        let interface_constructor: &Constructor = self
-            .interface_constructor_registry
-            .get(&interface_id)
-            .unwrap_or_else(||{
-                let interace_name: &str = type_name::<Interface>();
-                panic!("Interface {:?} with TypeId {:?} not registered to Container",interace_name,interface_id)
-            });
-
-        let any_box: Box<dyn Any> = interface_constructor(self);
-        let boxed_arc: Box<Arc<Interface>> = any_box
-            .downcast::<Arc<Interface>>()
-            .expect("Type mismatch during downcast");
-        let arc_trait: Arc<Interface> = *boxed_arc;
-        arc_trait
-    }
-
+    
     pub fn register<Interface,Implementation>(&mut self)
     where 
         Interface: ?Sized + 'static,
         Implementation: Injectable<Interface> + 'static,
     {
-        let interface_type: TypeId = TypeId::of::<Interface>();
+        let interface_id: TypeId = TypeId::of::<Interface>();
         let erased_constructor: Constructor = |container: &Container| -> Box<dyn Any> {
             let target: Arc<Interface> = Implementation::__syringe_construct(container);
             Box::new(target)
         };
-        self.interface_constructor_registry.insert(interface_type, erased_constructor);
+
+        let constructors = self.interface_constructor_registry
+            .entry(interface_id)
+            .or_insert_with(Vec::new);
+        constructors.push(erased_constructor);
+    }
+
+    pub fn get<Interface: ?Sized + 'static>(&self) -> Arc<Interface> {
+        let interface_id: TypeId = TypeId::of::<Interface>();
+        let interface_name: &str = type_name::<Interface>();
+        
+        let interface_constructor_array: &Vec<Constructor> = self
+            .interface_constructor_registry
+            .get(&interface_id)
+            .unwrap_or_else(||{
+                panic!(
+                    "Interface {:?} with TypeId {:?} not registered to Container",
+                    interface_name,
+                    interface_id
+                )
+            });
+
+        if interface_constructor_array.len() != 1 {
+            panic!(
+                "Container::get() expects one implementation for {:?} but received {:?}",
+                interface_name, 
+                interface_constructor_array.len(),
+            )
+        }
+        
+        let interface_constructor = interface_constructor_array
+            .first()
+            .unwrap_or_else(||{
+                panic!("Expected one implementation for {:?}",interface_name)
+            });
+        let any_box: Box<dyn Any> = interface_constructor(self);
+        let boxed_arc: Box<Arc<Interface>> = any_box
+            .downcast::<Arc<Interface>>()
+            .unwrap_or_else(|boxed_any|{
+                let any_interface_name: &str = type_name_of_val(&*boxed_any);
+                panic!(
+                    "Type mismatch during downcast. Expected: {:?} but received {:?}",
+                    interface_name,
+                    any_interface_name
+                )
+            });
+        let arc_instance: Arc<Interface> = *boxed_arc;
+        arc_instance
+    }
+
+    pub fn get_all<Interface: ?Sized + 'static>(&self) -> Vec<Arc<Interface>> {
+        let interface_id: TypeId = TypeId::of::<Interface>();
+        let interface_name: &str = type_name::<Interface>();
+        
+        let interface_constructor_array: &Vec<Constructor> = self
+            .interface_constructor_registry
+            .get(&interface_id)
+            .unwrap_or_else(||{
+                panic!(
+                    "Interface {:?} with TypeId {:?} not registered to Container",
+                    interface_name,
+                    interface_id
+                )
+            });
+
+        let all_instances: Vec<Arc<Interface>> = interface_constructor_array
+            .iter()
+            .map(|constructor: &Constructor| -> Arc<Interface> {
+                let any_box: Box<dyn Any> = constructor(self);
+                let boxed_arc: Box<Arc<Interface>> = any_box
+                    .downcast::<Arc<Interface>>()
+                    .unwrap_or_else(|boxed_any|{
+                        let any_interface_name: &str = type_name_of_val(&*boxed_any);
+                        panic!(
+                            "Type mismatch during downcast. Expected: {:?} but received {:?}",
+                            interface_name,
+                            any_interface_name
+                        )
+                    });
+                let arc_trait: Arc<Interface> = *boxed_arc;
+                return arc_trait
+            })
+            .collect::<Vec<Arc<Interface>>>();
+        
+        return all_instances;
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,30 @@
-use std::error::Error;
+use std::{error::Error, sync::Arc};
 
 use::syringe::container::Container;
-use syringe::test::user_repo::{UserMockRepo, UserPgRepo, UserRepo};
+use syringe::test::{user::User, user_repo::{UserMockRepo, UserPgRepo, UserRepo}};
 
 fn main() -> Result<(),Box<dyn Error>> {
     let mut container: Container = Container::new();
 
     container.register::<dyn UserRepo,UserMockRepo>();
-    let user_repo = container.get::<dyn UserRepo>();
-    let user1 = user_repo.find_one("".to_string());
-    println!("{:?}",user1);
+    container.register::<dyn UserRepo,UserPgRepo>();
+    let user_repos: Vec<Arc<dyn UserRepo>> = container.get_all::<dyn UserRepo>();
+    user_repos
+        .iter()
+        .for_each(|user_repo: &Arc<dyn UserRepo>| {
+            let user: User = user_repo.find_one("".to_string());
+            println!("{:?}",user)
+        });
 
     container.register::<UserMockRepo,UserMockRepo>();
     let mock_user_repo = container.get::<UserMockRepo>();
-    let user2 = mock_user_repo.find_one("".to_string());
-    println!("{:?}",user2);
+    let mock_user = mock_user_repo.find_one("".to_string());
+    println!("{:?}",mock_user);
 
     container.register::<UserPgRepo,UserPgRepo>();
     let pg_user_repo = container.get::<UserPgRepo>();
-    let user3 = pg_user_repo.find_one("".to_string());
-    println!("{:?}",user3);
+    let pg_user = pg_user_repo.find_one("".to_string());
+    println!("{:?}",pg_user);
 
     Ok(())
 }
